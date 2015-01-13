72cc7097d95a5e0e62f48a3805bc9f146ed66cff
amine <elhali.amine@gmail.com>
2015-01-13 12:40:22 +0000

    commit with conflicts

--- src/main/java/com/campany/git/training/App.java
+++ src/main/java/com/campany/git/training/App.java
@@ -10,7 +10,7 @@ public class App {
 
     public static void main(String... args) throws IOException {
         System.out.println("Hello Morocco");
-        System.out.println("Hi !");
+        System.out.println("hello!");
     }
 
 }
