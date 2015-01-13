2719b0fb9ae6e47ece87d35872027371a1469f98
amine <elhali.amine@gmail.com>
2015-01-13 11:54:38 +0000

    first commit

--- src/main/java/com/campany/git/training/App.java
+++ src/main/java/com/campany/git/training/App.java
@@ -2,18 +2,14 @@ package com.campany.git.training;
 
 import java.io.IOException;
 
-
 /**
  * Hello world!
  *
  */
-public class App 
-{
-
+public class App {
 
     public static void main(String... args) throws IOException {
-        System.out.println("Hello");
+        System.out.println("Hello Morocco");
     }
-
 
 }
