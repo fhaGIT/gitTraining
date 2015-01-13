2eddf769b59d9ae93a5eb536e204a66ddd2fe1b9
adil errady <errady.adil@gmail.com>
2015-01-13 12:47:49 +0100

    change hello Palestine

--- src/main/java/com/campany/git/training/App.java
+++ src/main/java/com/campany/git/training/App.java
@@ -9,7 +9,7 @@ import java.io.IOException;
 public class App {
 
     public static void main(String... args) throws IOException {
-        System.out.println("Hello Morocco");
+        System.out.println("Hello Palestine");
         System.out.println("Hi !");
     }
 
