bcfa3857e984121e304d2fa380be06a6ab0b743b
Fatima Zahra HALHOUL <fatimazahra.halhoul@nespresso.com>
2015-01-13 12:00:03 +0100

    init git project

---
+++ src/main/java/com/workshop/workshop/App.java
@@ -0,0 +1,19 @@
+package com.workshop.workshop;
+
+import java.io.IOException;
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+
+
+    public static void main(String... args) throws IOException {
+        System.out.println("Hello");
+    }
+
+
+}
